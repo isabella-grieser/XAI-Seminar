33d52227a80c69c8742acbd89112534dc9026819
isabella-grieser <isabella.grieser@yahoo.de>
2023-05-14 11:30:48 +0200

    add basic explainability

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from model import ExplainableModel
-import numpy as np
-import xgboost as xgb
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
+
 
 
 def get_data():
@@ -17,25 +16,30 @@ def get_data():
     x_train, y_train = train.drop(['isFraud'], axis=1), train["isFraud"]
     x_test, y_test = test.drop(['isFraud'], axis=1), test["isFraud"]
 
-    dtrain_reg = xgb.DMatrix(x_train, y_train, label=y_train, enable_categorical=True)
-    dtest_reg = xgb.DMatrix(x_test, y_test, label=y_train, enable_categorical=True)
-
-    return dtrain_reg, dtest_reg, y_test
+    return x_train, y_train, x_test, y_test
 
 
 if __name__ == '__main__':
-    dtrain_reg, dtest_reg, y_test = get_data()
+
+    x_train, y_train, x_test, y_test = get_data()
 
     params = {"objective": "binary:logistic",
-              "verbosity": 2,
+              "verbosity": 1,
               "tree_method": "gpu_hist"}
-    model = ExplainableModel(dtrain_reg, dtest_reg, params=params, path="./weights/model", seed=42)
-    model.train(iterations=300, do_train=True, load=False, save=True)
 
-    y_hat = model.predict(dtest_reg)
+    model = ExplainableModel(x_train, y_train, params=params,
+                             path="./weights/model.pkl",
+                             explainer_path="./weights/explainer.pkl",
+                             seed=42)
 
-    print(y_hat)
+    # model.train(iterations=300, do_train=False, load=True, save=False)
+
+    y_hat = model.predict(x_test)
+
     print("F1: " + str(f1_score(y_test, y_hat)))
     print("Precision: " + str(precision_score(y_test, y_hat)))
     print("Recall: " + str(recall_score(y_test, y_hat)))
     print("Accuracy: " + str(accuracy_score(y_test, y_hat)))
+
+    print("Explain Plot")
+    model.explain(x_test.iloc[0])
--- model.py
+++ model.py
@@ -2,16 +2,17 @@ import xgboost as xgb
 import shap
 import numpy as np
 import pickle
+import pandas as pd
 
 
 class ExplainableModel:
 
-    def __init__(self, dtrain, dtest, params,
-                 path="./weights/default.model",
+    def __init__(self, x, y, params,
+                 path="./weights/default.pkl",
                  explainer_path="./weights/explainer.pkl",
                  seed=42):
-        self.dtrain = dtrain
-        self.dtest = dtest
+        self.x = x
+        self.y = y
         self.params = params
         self.seed = seed
         self.path = path
@@ -21,41 +22,38 @@ class ExplainableModel:
 
     def train(self, iterations=300, early_stopping_rounds=10, do_train=True, load=False, save=True):
         if load:
-            self.model = xgb.Booster()
-            self.model.load_model(self.path)
+            self.model = pickle.load(open(self.path, 'rb'))
             self.explainer = pickle.load(open(self.explainer_path, 'rb'))
         if do_train:
-            evallist = [(self.dtrain, 'train'), (self.dtest, 'eval')]
-            self.model = xgb.train(self.params,
-                                   self.dtrain,
-                                   iterations,
-                                   evallist,
-                                   early_stopping_rounds=early_stopping_rounds)
+            self.model = xgb.XGBClassifier()
+            self.model.fit(self.x, self.y)
             self.explainer = shap.TreeExplainer(self.model)
         if save:
-            self.model.save_model(self.path)
+            with open(self.path, 'wb') as f:
+                pickle.dump(self. model, f)
             with open(self.explainer_path, 'wb') as f:
                 pickle.dump(self.explainer, f)
 
     def predict(self, x):
         if self.model is None:
-            self.model = xgb.Booster()
-            self.model.load_model(self.path)
+            self.model = pickle.load(open(self.path, 'rb'))
             self.explainer = pickle.load(open(self.explainer_path, 'rb'))
         return [round(value) for value in self.model.predict(x)]
 
     def predict_proba(self, x):
         if self.model is None:
-            self.model = xgb.Booster()
-            self.model.load_model(self.path)
+            self.model = pickle.load(open(self.path, 'rb'))
             self.explainer = pickle.load(open(self.explainer_path, 'rb'))
         return self.model.predict(x)
 
     def explain(self, x):
-        y = self.predict(x)
+        explain_val = x.to_numpy().reshape(1, -1)
+        y = self.predict(explain_val)
 
-        shap_values = self.explainer.shap_values(x)
+        shap_values = self.explainer.shap_values(explain_val)
         feature_importances = np.abs(shap_values).mean(0)
 
         print("Y is " + str(y))
-        print(feature_importances)
+        feature_zip = sorted(zip(self.x.columns, feature_importances), key=lambda t: t[1], reverse=True)
+        for c, v in feature_zip:
+            print(f"{c}: {v}")
